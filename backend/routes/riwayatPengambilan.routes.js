d59e47d72666162fdf92b5c1919595b223834976
Dolfis123 <kdolfis@gmail.com>
2025-06-07 01:50:55 +0900

    finish-83

--- backend/routes/riwayatPengambilan.routes.js
+++ backend/routes/riwayatPengambilan.routes.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const pengambilanController = require('./../controllers/pengambilanController');
+const statistikController = require('../controllers/pengambilanController');
 
 // Route untuk pengambilan barang
 router.post('/pengambilan', pengambilanController.pengambilanBarang);
 // Route untuk jumlah barang yang diambil hari, bulan, dan tahun ini
-router.get('/pengambilan/statistik', pengambilanController.getJumlahBarangDiambil);
+router.get('/pengambilan/statistik', statistikController.getJumlahBarangDiambil);
 
 // Route untuk get semua riwayat pengambilan
 router.get('/pengambilan', pengambilanController.getRiwayatPengambilan);

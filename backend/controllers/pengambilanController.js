cc75a5405d7fb715af6368313980adc7bdc18ef7
Dolfis123 <kdolfis@gmail.com>
2025-06-07 01:53:48 +0900

    finish-84

--- backend/controllers/pengambilanController.js
+++ backend/controllers/pengambilanController.js
@@ -66,125 +66,79 @@ const getRiwayatPengambilan = async (req, res) => {
     res.status(500).json({ message: 'Gagal mengambil data riwayat pengambilan.' });
   }
 };
+
+const { RiwayatPengambilan } = require('../models');
 const { Op } = require('sequelize');
-// const { RiwayatPengambilan } = require('../models');
 
+// Fungsi hitung jumlah barang diambil hari, bulan, tahun ini
+function getJumlahBarangDiambil(req, res) {
+  const now = new Date();
 
-exports.getJumlahBarangDiambil = async (req, res) => {
-  try {
-    const now = new Date();
+  // Hari ini
+  const awalHari = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
+  const akhirHari = new Date(awalHari);
+  akhirHari.setDate(akhirHari.getDate() + 1);
 
-    // Hari ini
-    const awalHari = new Date(now.setHours(0, 0, 0, 0));
-    const akhirHari = new Date(awalHari);
-    akhirHari.setDate(akhirHari.getDate() + 1);
+  // Bulan ini
+  const awalBulan = new Date(now.getFullYear(), now.getMonth(), 1);
+  const akhirBulan = new Date(now.getFullYear(), now.getMonth() + 1, 1);
 
-    // Bulan ini
-    const awalBulan = new Date(now.getFullYear(), now.getMonth(), 1);
-    const akhirBulan = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+  // Tahun ini
+  const awalTahun = new Date(now.getFullYear(), 0, 1);
+  const akhirTahun = new Date(now.getFullYear() + 1, 0, 1);
 
-    // Tahun ini
-    const awalTahun = new Date(now.getFullYear(), 0, 1);
-    const akhirTahun = new Date(now.getFullYear() + 1, 0, 1);
-
-    const [hariIni, bulanIni, tahunIni] = await Promise.all([
-      RiwayatPengambilan.sum('Jumlah_Diambil', {
-        where: {
-          Tanggal: {
-            [Op.gte]: awalHari,
-            [Op.lt]: akhirHari,
-          },
+  Promise.all([
+    RiwayatPengambilan.sum('Jumlah_Diambil', {
+      where: {
+        Tanggal: {
+          [Op.gte]: awalHari,
+          [Op.lt]: akhirHari,
         },
-      }),
-      RiwayatPengambilan.sum('Jumlah_Diambil', {
-        where: {
-          Tanggal: {
-            [Op.gte]: awalBulan,
-            [Op.lt]: akhirBulan,
-          },
+      },
+    }),
+    RiwayatPengambilan.sum('Jumlah_Diambil', {
+      where: {
+        Tanggal: {
+          [Op.gte]: awalBulan,
+          [Op.lt]: akhirBulan,
         },
-      }),
-      RiwayatPengambilan.sum('Jumlah_Diambil', {
-        where: {
-          Tanggal: {
-            [Op.gte]: awalTahun,
-            [Op.lt]: akhirTahun,
-          },
+      },
+    }),
+    RiwayatPengambilan.sum('Jumlah_Diambil', {
+      where: {
+        Tanggal: {
+          [Op.gte]: awalTahun,
+          [Op.lt]: akhirTahun,
         },
-      }),
-    ]);
-
+      },
+    }),
+  ])
+  .then(([hariIni, bulanIni, tahunIni]) => {
     res.status(200).json({
       hariIni: hariIni || 0,
       bulanIni: bulanIni || 0,
       tahunIni: tahunIni || 0,
     });
-  } catch (error) {
+  })
+  .catch((error) => {
     console.error('Gagal menghitung jumlah barang:', error);
     res.status(500).json({ message: 'Terjadi kesalahan server' });
-  }
-};
+  });
+}
+
+// Dummy fungsi pengambilanBarang
+function pengambilanBarang(req, res) {
+  res.status(200).json({ message: 'Pengambilan barang berhasil' });
+}
+
+// Dummy fungsi getRiwayatPengambilan
+function getRiwayatPengambilan(req, res) {
+  // Contoh data riwayat
+  res.status(200).json({ riwayat: [] });
+}
 
 module.exports = {
   pengambilanBarang,
-  getRiwayatPengambilan
-};
-
-const { Op } = require('sequelize');
-// const { RiwayatPengambilan } = require('../models');
-
-
-exports.getJumlahBarangDiambil = async (req, res) => {
-  try {
-    const now = new Date();
-
-    // Hari ini
-    const awalHari = new Date(now.setHours(0, 0, 0, 0));
-    const akhirHari = new Date(awalHari);
-    akhirHari.setDate(akhirHari.getDate() + 1);
-
-    // Bulan ini
-    const awalBulan = new Date(now.getFullYear(), now.getMonth(), 1);
-    const akhirBulan = new Date(now.getFullYear(), now.getMonth() + 1, 1);
-
-    // Tahun ini
-    const awalTahun = new Date(now.getFullYear(), 0, 1);
-    const akhirTahun = new Date(now.getFullYear() + 1, 0, 1);
-
-    const [hariIni, bulanIni, tahunIni] = await Promise.all([
-      RiwayatPengambilan.sum('Jumlah_Diambil', {
-        where: {
-          Tanggal: {
-            [Op.gte]: awalHari,
-            [Op.lt]: akhirHari,
-          },
-        },
-      }),
-      RiwayatPengambilan.sum('Jumlah_Diambil', {
-        where: {
-          Tanggal: {
-            [Op.gte]: awalBulan,
-            [Op.lt]: akhirBulan,
-          },
-        },
-      }),
-      RiwayatPengambilan.sum('Jumlah_Diambil', {
-        where: {
-          Tanggal: {
-            [Op.gte]: awalTahun,
-            [Op.lt]: akhirTahun,
-          },
-        },
-      }),
-    ]);
-
-    res.status(200).json({
-      hariIni: hariIni || 0,
-      bulanIni: bulanIni || 0,
-      tahunIni: tahunIni || 0,
-    });
-  } catch (error) {
-    console.error('Gagal menghitung jumlah barang:', error);
-    res.status(500).json({ message: 'Terjadi kesalahan server' });
-  }
+  getJumlahBarangDiambil,
+  getRiwayatPengambilan,
 };

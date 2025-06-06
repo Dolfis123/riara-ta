652fc3a0910df013dfe3ad588f6591fe7351f504
Dolfis123 <kdolfis@gmail.com>
2025-06-06 11:12:49 +0900

    finish-57

--- frontend/src/pages/Dashboard/Barang.jsx
+++ frontend/src/pages/Dashboard/Barang.jsx
@@ -264,7 +264,7 @@ const handleModalSubmit = async (e) => {
       className="w-full h-full object-contain"
     />
     <a
-      href={`https://skydance.life/public/qris/${barang.QR_Code}`} // Gabungkan base URL
+      href={barang.QR_Code} // Gabungkan base URL
       download={`QRCode_${barang.Nama_Barang}.png`}
       className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
     >

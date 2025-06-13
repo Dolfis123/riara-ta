35862a195a52f32dd8bc3690da6064d2e2098112
Dolfis123 <kdolfis@gmail.com>
2025-06-13 21:53:34 +0900

    RD-20

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://pengadilannegerimanokwari.pro", "https://www.pengadilannegerimanokwari.pro"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['skydance.life', 'www.skydance.life', 'localhost', '127.0.0.1']
+    allowedHosts: ['pengadilannegerimanokwari.pro', 'www.pengadilannegerimanokwari.pro', 'localhost', '127.0.0.1']
   }
 })

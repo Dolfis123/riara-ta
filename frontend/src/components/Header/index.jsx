d2533ce160d202f11f1a3c1094523314987d8db0
Dolfis123 <kdolfis@gmail.com>
2025-06-15 00:28:43 +0900

    RD-4

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -55,8 +55,8 @@ const Header = (props) => {
                     
         </div>
 
-        {/* <div className="hidden sm:block">
-          <form action="https://formbold.com/s/unique_form_id" method="POST">
+        <div className="hidden sm:block">
+          <form >
             <div className="relative">
               <button className="absolute left-0 top-1/2 -translate-y-1/2">
                 <svg
@@ -89,7 +89,7 @@ const Header = (props) => {
               />
             </div>
           </form>
-        </div> */}
+        </div>
 
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
